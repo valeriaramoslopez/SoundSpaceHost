c5f5f30e570407e0f73d467cb0935dbae68d4395
Elia Arteaga Delgado <delgadolia76@gmail.com>
2025-11-25 10:36:22 -0600

    Nombre en archivos

---
+++ server/Rutas/dbProductoRutas.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+    getProductos,
+    getProductosById,
+    getProductosByGenero,
+    createProducto,
+    updateProducto,
+    updateOferta,
+    deleteProducto //No se para que se usaria pero x
+} = require('../Controladores/dbProductoController');
+
+router.get('/', getProductos);
+router.get('/genero/:genero', getProductosByGenero);
+router.get('/:id', getProductosById);
+router.post('/createProducto', verificarToken, verificarAdmin, createProducto); //Tiene que estar protegida
+router.put('/oferta/:id', verificarToken, verificarAdmin, updateOferta);//Tiene que estar protegida
+router.put('/:id', verificarToken, verificarAdmin, updateProducto);//Tiene que estar protegida
+router.delete('/:id', verificarToken, verificarAdmin, deleteProducto);//Tiene que estar protegida
+
+module.exports = router;

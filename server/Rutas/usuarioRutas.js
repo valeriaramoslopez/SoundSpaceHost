98c237c21e5202155ece0f9216ca975366d8a882
Elia Arteaga Delgado <delgadolia76@gmail.com>
2025-11-30 15:49:36 -0600

    Grafica y api modificacion

--- server/Rutas/usuarioRutas.js
+++ server/Rutas/usuarioRutas.js
@@ -4,5 +4,6 @@ const usuarioControlador = require("../Controladores/usuarioControlador");
 
 router.post("/registrar", usuarioControlador.registrarUsuario);
 router.post("/login", usuarioControlador.loginUsuario);
+router.post("/recuperar", usuarioControlador.recuperarContrasena);
 
 module.exports = router;

3250fed183e4b252518b24847750b1bef7e2313e
Darely Quezada <darelyqg@gmail.com>
2025-12-03 12:02:23 -0600

    Fix: Fixing verifyToken in nota

--- server/Rutas/nota.routes.js
+++ server/Rutas/nota.routes.js
@@ -4,6 +4,6 @@ const notaController = require('../Controladores/nota.controller');
 const verificarToken = require('../Middleware/verificarToken');
 
 // POST /api/nota/compra - generar PDF y enviar nota de compra
-router.post('/compra', notaController.enviarNotaCompra);
+router.post('/compra', verificarToken, notaController.enviarNotaCompra);
 
 module.exports = router;
